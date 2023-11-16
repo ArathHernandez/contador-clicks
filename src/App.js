85a134996b95ecc3a7e08f0debac6ba26eec4d28
ArathHernandez <earathdzv14@gmail.com>
2023-11-16 17:59:19 -0600

    ClicksCounter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import tecLogo from './imagenes/tec.png'
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='tec-logo-contenedor'>
+        <img 
+          className='tec-logo'
+          src={tecLogo}
+          alt='logo del Tec'
+        />
+      </div>
+      <div className='contenedor-principal'>
+          <Contador numClicks={numClicks}/>
+          <Boton
+          texto='Click'
+          esBotonClick={true}
+          manejarClick={manejarClick}
+          />
+          <Boton
+          texto='Reiniciar'
+          esBotonClick={false}
+          manejarClick={reiniciarContador}
+          />
+      </div>
     </div>
   );
 }
